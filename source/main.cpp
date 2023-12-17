b75e7a1a32b253f7cca9d33c801dbf9b0c88ebd0
René Hansen <rene@hansen-audio.de>
2023-12-17 17:29:02 +0100

    Add run_sync and run_async functions for testing and demonstration

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,69 @@
 // Copyright Max & Me 2023
 
 #include "mam/meta_words/runner.h"
+#include <future>
+#include <iostream>
+#include <thread>
 
 using namespace mam::meta_words;
 
+//-----------------------------------------------------------------------------
+void print_result(const MetaWords& meta_words)
+{
+    for (auto& word : meta_words)
+    {
+        std::cout << word.word;
+    }
+}
+
+//-----------------------------------------------------------------------------
+void run_sync(Command& cmd)
+{
+    std::cout << "Run sync..." << '\n';
+
+    FnProgress fn_progress = [](ProgressValue val) { double tmp = val; };
+
+    print_result(run(cmd, fn_progress));
+}
+
+//-----------------------------------------------------------------------------
+void run_async(Command& cmd)
+{
+    std::cout << "Run async..." << '\n';
+
+    std::future<MetaWords> future_handle = std::async([&]() {
+        FnProgress fn_progress = [](ProgressValue val) { double tmp = val; };
+
+        return run(cmd, fn_progress);
+    });
+
+    // Our idle loop...
+    while (true)
+    {
+        std::cout << "Idle timer..." << '\n';
+        // '0': ask for the future state immediately. There is no waiting!
+        if (future_handle.wait_for(std::chrono::seconds(0)) ==
+            std::future_status::ready)
+            break;
+
+        // Simulate the ilde timer
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    }
+
+    print_result(future_handle.get());
+}
+
+//-----------------------------------------------------------------------------
 int main()
 {
     // whisper.cpp executable
     const PathType executable =
         "/mnt/veracrypt2/Developer/github/whisper_test/build/bin/"
         "main";
-    //  The whisper.cpp library takes the audio file and writes the result of
-    //  its analysis into a CSV file. The file is named like the audio file and
-    //  by prepending ".csv" e.g. my_speech.wav -> my_speech.wav.csv
+    //  The whisper.cpp library takes the audio file and writes the result
+    //  of its analysis into a CSV file. The file is named like the audio
+    //  file and by prepending ".csv" e.g. my_speech.wav ->
+    //  my_speech.wav.csv
     const Options options         = {"-ocsv"};
     const OneValArgs one_val_args = {
         // model file resp. binary
@@ -26,13 +77,8 @@ int main()
 
     Command cmd{executable, options, one_val_args};
 
-    FnProgress fn_progress = [](ProgressValue val) {
-        // https://github.com/openai/whisper/discussions/850
-        // TODO: Does not work ...yet!
-        double tmp = val;
-    };
-
-    const auto meta_words = run(cmd, fn_progress);
+    run_sync(cmd);
+    run_async(cmd);
 
     return 0;
 }
